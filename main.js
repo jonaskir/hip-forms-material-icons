9b0ab4dd621df86ae679dbaf28f1fe0a72ca9250
Jonas Kirchhoff <jonaskir@mail.uni-paderborn.de>
2017-01-10 22:22:31 +0100

    Adds promises for better error handling.

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ const path = require('path');
 const unzipper = require('unzipper');
 const program = require('commander');
 
+// constants
+const baseUrl = 'https://storage.googleapis.com/material-icons/external-assets/v4/icons/zip/';
+const tempDir = path.join(__dirname, 'tmp');
+
+// commandline setup
 program 
   .version('1.0.0')
   .usage('[options] "<Material Icon Name>"')
@@ -23,14 +28,10 @@ program.on('--help', function () {
 program.parse(process.argv);
 
 // terminate if no HiP-Forms directory is specified.
-if (!process.dir || process.dir.length <= 0) {
+if (!program.dir || program.dir.length <= 0) {
   console.error('ERROR! You need to specify the HiP-Forms directory!');
   process.exit(1)
 }
-
-// constants
-const baseUrl = 'https://storage.googleapis.com/material-icons/external-assets/v4/icons/zip/';
-const tempDir = path.join(__dirname, 'tmp');
 
 let iconArg = program.args.join(' ');
 
@@ -47,70 +48,142 @@ function getIcFilename(ext, color, size) {
   return filename;
 }
 
-// Moves icons for Android to correct location.
-function processAndroid() {
-  console.log('Processing icons for Android');
-  ['drawable-hdpi', 'drawable-mdpi', 'drawable-xhdpi', 'drawable-xxhdpi', 'drawable-xxxhdpi'].forEach((drawableDir) => {
-    let origFile = path.join(tempDir, getIcFilename(null, program.color, program.size), 'android', drawableDir, getIcFilename('png', program.color, program.size));
-    let dest = path.join(program.dir, 'Sources\\HipMobileUI.Droid\\Resources', drawableDir, getIcFilename('png'));
-    console.log('\tCreating', dest);
-    copyFile(origFile, dest);
+// Downloads the ZipFile corresponding to the specified icon.
+// Returns a Promise which resolves with the path of the downloaded Zip file.
+function downloadZipFile() {
+  let zipFilename = getIcFilename('zip', program.color, program.size);
+  let downloadUrl = baseUrl + zipFilename;
+  let downloadDest = path.join(tempDir, zipFilename);
+  console.log(`Downloading "${iconArg}" from ${downloadUrl}`);
+
+  return new Promise((resolve, reject) => {
+    let file = fs.createWriteStream(downloadDest);
+
+    file.on('error', (err) => {
+      reject(err);
+    });
+
+    https.get(downloadUrl, (response) => {
+      response.pipe(file);
+
+      file.on('finish', () => {
+        console.log('Download finished.');
+        resolve(downloadDest);
+      });
+    });
   });
-  console.log('Processing icons for Android finished');
 }
 
-// Moves icons for iOS to correct location.
-function processIos() {
-  console.log('Processing icons for iOS');
+// Unzips the specified zip file.
+// Returns a promise resolving with the path where the content of the zip file was extracted to.
+function unzipZipFile(filepath) {
+  console.log('Extracting icons from zip file ...')
+  return new Promise((resolve, reject) => {
+    let zipFile = fs.createReadStream(filepath);
+    zipFile.on('error', (err) => {
+      reject(err);
+    });
+
+    let targetPath = tempDir;
+    let target = unzipper.Extract({ path: tempDir });
+    target.on('error', reject);
+    target.on('finish', () => {
+      console.log('Extraction finished.')
+      fs.unlink(filepath, (err) => {
+        if (err)
+          reject(err);
+        else
+          resolve(path.join(targetPath, path.basename(filepath, path.extname(filepath))));
+      });
+    });
+
+    zipFile.pipe(target);
+  });
+}
+
+// Copies the file identified by srcPath to a new file identified by destPath.
+function copyFile(srcPath, destPath) {
+  return new Promise((resolve, reject) => {
+    let src = fs.createReadStream(srcPath);
+    src.on('error', reject);
+
+    let dest = fs.createWriteStream(destPath);
+    dest.on('error', reject);
+
+    console.log('\tCreating', destPath);
+    src.pipe(dest);
+
+    dest.on('finish', resolve);
+  });
+}
+
+// Copies the Android icons from the specified directory into the HiP-Forms Solution.
+// Returns a promise resolving with the path where the content of the zip file was extracted to.
+function copyAndroidIcons(dir) {
+  console.log('Copying Android icons ...');
+  let copyPromises = [];
+  ['drawable-hdpi', 'drawable-mdpi', 'drawable-xhdpi', 'drawable-xxhdpi', 'drawable-xxxhdpi'].forEach((drawableDir) => {
+    let src = path.join(dir, 'android', drawableDir, getIcFilename('png', program.color, program.size));
+    let dest = path.join(program.dir, 'Sources\\HipMobileUI.Droid\\Resources', drawableDir, getIcFilename('png'));
+    copyPromises.push(copyFile(src, dest));
+  });
+  return Promise.all(copyPromises)
+  .then(() => { 
+    console.log('Copying Android icons finished.'); 
+    return dir;
+  });
+}
+
+// Copies the iOS icons from the specified directory into the HiP-Forms Solution.
+// Returns a promise resolving with the path where the content of the zip file was extracted to.
+function copyiOSIcons(dir) {
+  console.log('Copying iOS icons ...');
+  let copyPromises = [];
   ['', '_2x', '_3x'].forEach((version) => {
     let imageset = (program.color === 'black')? getIcFilename() : getIcFilename(null, program.color);
     let color = (program.color === 'black')? '': '_white';
     let size = (program.size === 24)? '' : `_${program.size}pt`;
-    let origFile = path.join(tempDir, getIcFilename(null, program.color, program.size), 'ios', imageset + size + '.imageset', getIcFilename() + color + size + version + '.png');
+    let src = path.join(dir, 'ios', imageset + size + '.imageset', getIcFilename() + color + size + version + '.png');
     let dest = path.join(program.dir, 'Sources\\HipMobileUI.iOS\\Resources', getIcFilename() + version.replace('_', '@') + '.png');
-    console.log('\tCreating', dest);
-    copyFile(origFile, dest);
+    copyPromises.push(copyFile(src, dest));
   })
-  console.log('Processing icons for iOS finished');
-}
-
-// Copies file from src path to dest path.
-// see: http://stackoverflow.com/questions/11293857/fastest-way-to-copy-file-in-node-js
-function copyFile(src, dest) {
-  fs.createReadStream(src).pipe(fs.createWriteStream(dest));
-}
-
-// download
-let zipFilename = getIcFilename('zip', program.color, program.size);
-let downloadUrl = baseUrl + zipFilename;
-let downloadDest = path.join(tempDir, zipFilename);
-console.log(`Downloading "${iconArg}" from ${downloadUrl}`);
-
-var file = fs.createWriteStream(downloadDest);
-var request = https.get(downloadUrl, function(response) {
-  response.pipe(file);
-  file.on('finish', function() {
-    console.log('Download finished.');
-
-    // extract
-    console.log('Extracting zip file ...');
-    fs.createReadStream(downloadDest).pipe(unzipper.Extract({ path: tempDir }))
-    .on('close', () => {
-      console.log('Extraction finished.');
-
-      // move files
-      console.log('Moving files to project located at', program.dir);
-      processAndroid();
-      processIos();
-
-      // delete zip & extracted folder
-      fs.unlink(downloadDest, () => {});
-
-      console.log("\nAll done! Don't forget to add the files to the project using Solution Explorer > Show All Files.");
-    });
-    
+  return Promise.all(copyPromises)
+  .then(() => { 
+    console.log('Copying iOS icons finished.');
+    return dir;
   });
-}).on('error', function(err) { // handle download errors
+}
+
+// Performs cleanup by deleting temporarily extracted contents of zip file.
+function cleanup(dir) {
+  console.log('Removing temporarily extracted zip contents ...');
+
+  // cf. http://stackoverflow.com/questions/18052762/remove-directory-which-is-not-empty
+  var deleteFolderRecursive = function(path) {
+    if( fs.existsSync(path) ) {
+      fs.readdirSync(path).forEach(function(file,index){
+        var curPath = path + "/" + file;
+        if(fs.lstatSync(curPath).isDirectory()) { // recurse
+          deleteFolderRecursive(curPath);
+        } else { // delete file
+          fs.unlinkSync(curPath);
+        }
+      });
+      fs.rmdirSync(path);
+    }
+  };
+
+  deleteFolderRecursive(dir);
+  return Promise.resolve();
+}
+
+downloadZipFile()
+.then(unzipZipFile)
+.then(copyAndroidIcons)
+.then(copyiOSIcons)
+.then(cleanup)
+.then(() => console.log('Copying finished successfully! Remember to add the files to the project using Solution Explorer > Show All Files.'))
+.catch((err) => {
+  console.error(`\n !!! An error occured: !!!\n`)
   console.error(err);
-  fs.unlink(downloadDest, () => {}); // Delete the file 
 });
